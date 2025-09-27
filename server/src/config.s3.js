aacad7a0f0252be9ec5451e75415759d31a9c1d9
Aswanth Raj <81868382+aswanthraj2112@users.noreply.github.com>
2025-09-28 05:04:15 +1000

    feat: move video storage to s3 and dynamodb

---
+++ server/src/config.s3.js
@@ -0,0 +1,31 @@
+import { SSMClient, GetParameterCommand } from '@aws-sdk/client-ssm';
+
+const REGION = process.env.AWS_REGION || 'ap-southeast-2';
+const ssm = new SSMClient({ region: REGION });
+const paramCache = new Map();
+let configPromise;
+
+async function getParam (Name) {
+  if (paramCache.has(Name)) {
+    return paramCache.get(Name);
+  }
+  const { Parameter } = await ssm.send(new GetParameterCommand({ Name }));
+  paramCache.set(Name, Parameter.Value);
+  return Parameter.Value;
+}
+
+export async function loadS3Config () {
+  if (!configPromise) {
+    configPromise = (async () => ({
+      REGION,
+      S3_BUCKET: await getParam('/n11817143/app/s3Bucket'),
+      RAW_PREFIX: await getParam('/n11817143/app/s3_raw_prefix'),
+      TRANSCODED_PREFIX: await getParam('/n11817143/app/s3_transcoded_prefix'),
+      THUMB_PREFIX: await getParam('/n11817143/app/s3_thumbnail_prefix'),
+      PRESIGNED_TTL_SECONDS: Number.parseInt(
+        await getParam('/n11817143/app/presigned_ttl_seconds'), 10
+      )
+    }))();
+  }
+  return configPromise;
+}
